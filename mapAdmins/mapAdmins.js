e56521518158138a75e0bcd63e0f1c932edd826a
Diego Apolinario <dieego.apolinario@gmail.com>
2022-07-27 17:07:05 -0300

    create mapAdmins and setArray

---
+++ mapAdmins/mapAdmins.js
@@ -0,0 +1,18 @@
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Stephany', 'Admin');
+usuarios.set('Jorge', 'User');
+usuarios.set('Natália', 'Admin');
+
+function getAdmins(map){
+  let admins = [];
+  for([key, value] of map){
+    if(value === 'Admin'){
+      admins.push(key);
+    }
+  }
+  return admins;
+}
+
+console.log(getAdmins(usuarios));
