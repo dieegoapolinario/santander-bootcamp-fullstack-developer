480697c230e62a116606e36aee1e2be7a1f3a757
Diego Apolinario <dieego.apolinario@gmail.com>
2022-07-28 14:04:52 -0300

    create modulos

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Diego', 31));
+console.log(mostraCidade('Diego', 'São Paulo'));
+console.log(mostraHobby('Diego', 'jogar video game'));
