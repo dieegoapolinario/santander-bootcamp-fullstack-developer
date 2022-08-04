94902dace7adcaa191a96a68deb6c1025e87723b
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-04 18:42:34 -0300

    commit diretivas

--- Angular/ambiente-angular/src/app/app.component.ts
+++ Angular/ambiente-angular/src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   count = 0;
+  nome = "Diego Apolinário";
+  text = '';
 
-  constructor(){}
+  pessoas = [
+    {
+      nome: 'Diego',
+      sobrenome: 'Apolinário',
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silva',
+    },
+    {
+      nome: 'João',
+      sobrenome: 'Pereira',
+    },
+    {
+      nome: 'Marcio',
+      sobrenome: 'Santos',
+    },
+  ];
 
-  
+  constructor() {}
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  clicou(nome: string): void{
+    console.log('Clicou!!!', nome)
+  }
 }
