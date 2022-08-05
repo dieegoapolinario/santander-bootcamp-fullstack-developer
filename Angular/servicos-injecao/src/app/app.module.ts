c1208a2585bd2a38e7d21265f47689a8beabdc16
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-04 21:10:50 -0300

    criando serviço para acessar a api

--- Angular/servicos-injecao/src/app/app.module.ts
+++ Angular/servicos-injecao/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
 import { PeopleService } from './shared/services/people.service';
 import { ListPeopleComponent } from './shared/components/list-people/list-people.component';
+import { ListApiComponent } from './shared/components/list-api/list-api.component';
 
 @NgModule({
-  declarations: [AppComponent, MarcaTextoDirective, ListPeopleComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MarcaTextoDirective,
+    ListPeopleComponent,
+    ListApiComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [PeopleService],
   bootstrap: [AppComponent],
 })
