38ec734b1cd97972278526cb444d9c628becb16b
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-06 13:09:08 -0300

    commit dados entre components

--- Angular/crud-angular/src/app/app.module.ts
+++ Angular/crud-angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UsersListComponent } from './pages/users/users-list/users-list.componen
 import { UserFormComponent } from './pages/users/user-form/user-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { DataBindingComponent } from './pages/data-binding/data-binding.componen
     UsersListComponent,
     UserFormComponent,
     DataBindingComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
