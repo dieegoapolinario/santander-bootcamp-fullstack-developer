f825eccf4b7f85719e43f150df4c64ef72be55ad
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-06 17:05:19 -0300

    salvando e recuperando dados salvo localmente

--- Angular/crud-angular/src/app/pages/todo/todo-list/todo-list.component.ts
+++ Angular/crud-angular/src/app/pages/todo/todo-list/todo-list.component.ts
@@ -13,6 +13,13 @@ export class TodoListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let items: any = localStorage.getItem('todos');
+    let todos = JSON.parse(items);
+    if(!todos){
+      this.todos = []
+    } else{
+      this.todos = todos
+    }
   }
 
   addTodo(title: string){
@@ -22,11 +29,12 @@ export class TodoListComponent implements OnInit {
       title: title,
       done: false
     });
-    console.log(this.todos)
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   removeTodo(todo: any){
     let index = this.todos.indexOf(todo);
-    this.todos.splice(index, 1)
+    this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
