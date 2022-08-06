d91fd49130562f85723cece3b5b2619b40a9c13d
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-06 14:15:48 -0300

    create todo-list

--- Angular/crud-angular/src/app/app.module.ts
+++ Angular/crud-angular/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
 import { UserFormComponent } from './pages/users/user-form/user-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { ButtonComponent } from './components/button/button.component';
 import { LifeCycleComponent } from './components/life-cycle/life-cycle.component';
+import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
     DataBindingComponent,
     ButtonComponent,
     LifeCycleComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
