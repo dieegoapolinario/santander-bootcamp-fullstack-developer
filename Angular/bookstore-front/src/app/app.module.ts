9832c87140ee8d9804d2771a1c90b638b379b07c
Diego Apolinario <dieego.apolinario@gmail.com>
2022-08-07 01:28:39 -0300

    update bookstore

--- Angular/bookstore-front/src/app/app.module.ts
+++ Angular/bookstore-front/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
@@ -10,6 +11,7 @@ import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.
 import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
 import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
 import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+import { BookService } from './components/bookstore-app/product-list/product-list.component.service';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,10 @@ import { ProductItemComponent } from './components/bookstore-app/product-list/pr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/bookstore-front/src/app/components/bookstore-app/product-list/product-list.component.service.ts
+++ Angular/bookstore-front/src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Book } from './model/book';
+//import { Book } from './model/book';
 
 @Injectable()
-
-export class BookService{
+export class BookService {
   private url = 'https://localhost:44382/api/bookstore';
 
   httpOptions = {
-    Headers: new HttpHeaders({'content-type': 'application/json'});
+    Headers: new HttpHeaders({ 'content-type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getBook() {
+    return this.http.get(this.url);
   }
-  
-  constructor(private http:HttpClient){}
 }
